7f805016e806bf613c4eb5ddcf4270d378f6cabd
somnath <yadavdada72@gmail.com>
2025-09-26 14:26:16 +0530

    Reference Variables

--- OOP_CPP/OOP_CPP.cpp
+++ OOP_CPP/OOP_CPP.cpp
@@ -2,6 +2,7 @@
 #include <string>
 int main()
 {
+    /*Input & Output*/
     int a, b;
     std::string str;
     std::cout << "\nPlease enter your name \n";
@@ -13,4 +14,9 @@ int main()
     std::cout << "\nb = ";
     std::cin >> b;
     std::cout << "\nAddition = " << a + b;
+
+    /*Reference Variable*/
+    int c = 100;
+    int &c_ref = c;
+    std::cout << "\n\nReference Variables\nc : " << c << "\nc_ref = " << c_ref;
 }
